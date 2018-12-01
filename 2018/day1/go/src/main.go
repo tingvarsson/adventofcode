2a7e31d96ca7de802112c72ad4788a9262e39117
Thomas Ingvarsson <ingvarsson.thomas@gmail.com>
2018-12-01 22:13:57 +0100

    [day1] Hack in go and python, possibly rust if time allows it

---
+++ 2018/day1/go/src/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("../input.txt")
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var numbers []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		i, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, i)
+	}
+
+	sum := 0
+	for _, n := range numbers {
+		sum += n
+	}
+	println(sum)
+
+	var knownsums []int
+	sum = 0
+	idx := 0
+	for {
+		sum += numbers[idx%len(numbers)]
+		pos := sort.Search(len(knownsums), func(i int) bool { return knownsums[i] >= sum })
+		if pos < len(knownsums) && knownsums[pos] == sum {
+			break
+		} else {
+			knownsums = append(knownsums, 0)
+			copy(knownsums[pos+1:], knownsums[pos:])
+			knownsums[pos] = sum
+			idx++
+		}
+	}
+	println(sum)
+}
---
+++ 2018/day1/python/main.py
@@ -0,0 +1,25 @@
+import bisect
+
+f = open("../input.txt", "r")
+fl = f.readlines()
+numbers = []
+for l in fl:
+    numbers.append(int(l))
+
+sum = 0
+for n in numbers:
+    sum += n
+print(sum)
+
+sum = 0
+i = 0
+knownsums = [0]
+while True:
+    sum += numbers[i % len(fl)]
+    pos = bisect.bisect_left(knownsums, sum)
+    if pos != len(knownsums) and knownsums[pos] == sum:
+        break
+    else:
+        bisect.insort(knownsums, sum)
+        i += 1
+print(sum)
