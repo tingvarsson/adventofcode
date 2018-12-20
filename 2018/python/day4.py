1e66bc4c81ce2ba585423b3e2f115672dfeb479f
Thomas Ingvarsson <ingvarsson.thomas@gmail.com>
2018-12-20 20:34:15 +0100

    [python][day4] Utilize utils, add main(), and simplify re usage

--- 2018/python/day4.py
+++ 2018/python/day4.py
@@ -1,42 +1,39 @@
 import bisect
 import re
+import utils
 
-# We preprocess the list with a simple cli call to sort
-f = open("day4/sortedinput", "r")
-lines = f.read().splitlines()
 
-guardIdRegex = r'#(\d+)'
-guardIdPattern = re.compile(guardIdRegex)
-asleepRegex = r':(\d+)] falls asleep'
-asleepPattern = re.compile(asleepRegex)
-wakesRegex = r':(\d+)] wakes up'
-wakesPattern = re.compile(wakesRegex)
+def main():
+    # We preprocess the list with a simple cli call to sort
+    lines = utils.readlines("day4/sortedinput")
 
-sleeptime = 60
+    guards = {}
+    for line in lines:
+        guardIdMatch = re.search(r"#(\d+)", line)
+        if guardIdMatch is not None:
+            guardId = int(guardIdMatch.group(1))
+            if guardId in guards:
+                currentSleepRecord = guards[guardId]
+            else:
+                currentSleepRecord = [0] * 60
+                guards[guardId] = currentSleepRecord
 
-guards = {}
-for line in lines:
-    guardIdMatch = guardIdPattern.search(line)
-    if guardIdMatch is not None:
-        guardId = int(guardIdMatch.group(1))
-        if guardId in guards:
-            currentSleepRecord = guards[guardId]
-        else:
-            currentSleepRecord = [0] * sleeptime
-            guards[guardId] = currentSleepRecord
+        asleepMatch = re.search(r":(\d+)] falls asleep", line)
+        if asleepMatch is not None:
+            asleepMinute = int(asleepMatch.group(1))
 
-    asleepMatch = asleepPattern.search(line)
-    if asleepMatch is not None:
-        asleepMinute = int(asleepMatch.group(1))
+        awakeMatch = re.search(r":(\d+)] wakes up", line)
+        if awakeMatch is not None:
+            awakeMinute = int(awakeMatch.group(1))
+            for t in range(asleepMinute, awakeMinute):
+                currentSleepRecord[t] += 1
 
-    wakesMatch = wakesPattern.search(line)
-    if wakesMatch is not None:
-        wakesMinute = int(wakesMatch.group(1))
-        for t in range(asleepMinute, wakesMinute):
-            currentSleepRecord[t] += 1
+    scenarioOneGuard = max(guards.items(), key=lambda g: sum(g[1]))
+    print(scenarioOneGuard[0] * scenarioOneGuard[1].index(max(scenarioOneGuard[1])))
 
-scenarioOneGuard = max(guards.items(), key=lambda g: sum(g[1]))
-print(scenarioOneGuard[0] * scenarioOneGuard[1].index(max(scenarioOneGuard[1])))
+    scenarioTwoGuard = max(guards.items(), key=lambda g: max(g[1]))
+    print(scenarioTwoGuard[0] * scenarioTwoGuard[1].index(max(scenarioTwoGuard[1])))
 
-scenarioTwoGuard = max(guards.items(), key=lambda g: max(g[1]))
-print(scenarioTwoGuard[0] * scenarioTwoGuard[1].index(max(scenarioTwoGuard[1])))
+
+if __name__ == "__main__":
+    main()
