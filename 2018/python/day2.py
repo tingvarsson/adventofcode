40d8752f9681c4a91fed80c85a8650af6603569c
Thomas Ingvarsson <ingvarsson.thomas@gmail.com>
2018-12-20 20:18:51 +0100

    [python][day2] Use utils.py and do some basic cleanup

--- 2018/python/day2.py
+++ 2018/python/day2.py
@@ -1,42 +1,45 @@
-f = open("day2/input", "r")
-fl = f.read().splitlines()
+import utils
 
-sum2 = 0
-sum3 = 0
-for line in fl:
-    letters = {}
-    for c in line:
-        if c in letters:
-            letters[c] += 1
-        else:
-            letters[c] = 1
-    
-    found2 = False
-    found3 = False
-    for c in letters:
-        if letters[c] == 2:
-            found2 = True
-        elif letters[c] == 3:
-            found3 = True
-    
-    if found2:
-        sum2 += 1
-    if found3:
-        sum3 += 1
-
-print("checksum:")
-print(sum2*sum3)
 
 def strIntersection(s1, s2):
     out = ""
-    for i in range(0, len(s1)):
+    for i in range(len(s1)):
         if s1[i] == s2[i]:
             out += s1[i]
     return out
 
-for line in fl:
-    for secondline in fl:
-        if len(line)-1 == len(strIntersection(line, secondline)):
-            print("same line:")
-            print(strIntersection(line, secondline))
-            exit(0)
+
+def main():
+    lines = utils.readlines("day2/input")
+
+    sum2 = 0
+    sum3 = 0
+    for line in lines:
+        letters = {}
+        for c in line:
+            if c in letters:
+                letters[c] += 1
+            else:
+                letters[c] = 1
+
+        found2 = False
+        found3 = False
+        for c in letters:
+            if not found2 and letters[c] == 2:
+                sum2 += 1
+                found2 = True
+            elif not found3 and letters[c] == 3:
+                sum3 += 1
+                found3 = True
+
+    print("Checksum:", sum2 * sum3)
+
+    for line in lines:
+        for secondline in lines:
+            if len(line) - 1 == len(strIntersection(line, secondline)):
+                print("Common letters:", strIntersection(line, secondline))
+                return
+
+
+if __name__ == "__main__":
+    main()
