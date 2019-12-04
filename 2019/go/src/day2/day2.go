f1356cd22bdccca7354dcbb4a01036d324998467
Ingvarsson Thomas <thomas.ingvarsson@tieto.com>
2019-12-04 06:40:15 +0100

    [19day2][go] Fix tests, extract noun/verb logic

--- 2019/go/src/day2/day2.go
+++ 2019/go/src/day2/day2.go
@@ -6,9 +6,7 @@ import (
 	"utils"
 )
 
-func runProg(memory []int, noun int, verb int) int {
-	memory[1] = noun
-	memory[2] = verb
+func runProg(memory []int) int {
 	for i := 0; memory[i] != 99; i += 4 {
 		if memory[i] == 1 {
 			memory[memory[i+3]] = memory[memory[i+1]] + memory[memory[i+2]]
@@ -27,11 +25,17 @@ func run(filepath string) (pos0 int, noun int, verb int) {
 		intcode = append(intcode, utils.Atoi(code))
 	}
 
-	pos0 = runProg(append([]int(nil), intcode...), 12, 2)
+	memory := append([]int(nil), intcode...)
+	memory[1] = 12
+	memory[2] = 2
+	pos0 = runProg(memory)
 
 	for noun = 0; noun < 100; noun++ {
 		for verb = 0; verb < 100; verb++ {
-			currentPos0 := runProg(append([]int(nil), intcode...), noun, verb)
+			memory := append([]int(nil), intcode...)
+			memory[1] = noun
+			memory[2] = verb
+			currentPos0 := runProg(memory)
 			if currentPos0 == 19690720 {
 				return
 			}
--- 2019/go/src/day2/day2_test.go
+++ 2019/go/src/day2/day2_test.go
@@ -4,19 +4,39 @@ import (
 	"testing"
 )
 
-func TestRun(t *testing.T) {
+func TestRunProg(t *testing.T) {
 	var tests = []struct {
-		input    string
+		input    []int
 		expected int
 	}{
-		{"../../../day2/example", 3500},
-		{"../../../day2/example2", 30},
-		{"../../../day2/input", 4023471},
+		{[]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50}, 3500},
+		{[]int{1, 1, 1, 4, 99, 5, 6, 0, 99}, 30},
+	}
+
+	for _, test := range tests {
+		if output := runProg(test.input); output != test.expected {
+			t.Errorf("Test Failed: %v input, %v expected, recieved: %v", test.input, test.expected, output)
+		}
+	}
+}
+
+func TestRun(t *testing.T) {
+	var tests = []struct {
+		input     string
+		expected  int
+		expected2 int
+		expected3 int
+	}{
+		{"../../../day2/input", 4023471, 80, 51},
 	}
 
 	for _, test := range tests {
 		if o1, o2, o3 := run(test.input); o1 != test.expected {
-			t.Errorf("Test Failed: %v input, %v expected, recieved: %v %v %v", test.input, test.expected, o1, o2, o3)
+			t.Errorf("Test Failed: %v input, %v %v %v expected, recieved: %v %v %v", test.input, test.expected, test.expected2, test.expected3, o1, o2, o3)
 		}
 	}
+}
+
+func TestMain(t *testing.T) {
+	main()
 }
