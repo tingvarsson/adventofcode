91ae670ae6ae41c3404037efdecca92da7dd8efb
Thomas Ingvarsson <ingvarsson.thomas@gmail.com>
2019-12-15 21:45:01 +0100

    [19day5][go] Refactor day5 to use program

--- go/src/2019/day5/day5.go
+++ go/src/2019/day5/day5.go
@@ -3,82 +3,31 @@ package main
 import (
 	"fmt"
 	"os"
+	"program"
 	"strings"
 	"utils"
 )
 
-func getValue(memory []int, param int, mode int) int {
-	if mode == 0 {
-		return memory[param]
-	}
-
-	return param
-}
-
-func runProg(memory []int, input int) (output int) {
-	i := 0
-	for true {
-		op := memory[i] % 100
-		m1 := (memory[i] / 100) % 10
-		m2 := (memory[i] / 1000) % 10
-		if op == 1 {
-			memory[memory[i+3]] = getValue(memory, memory[i+1], m1) + getValue(memory, memory[i+2], m2)
-			i += 4
-		} else if op == 2 {
-			memory[memory[i+3]] = getValue(memory, memory[i+1], m1) * getValue(memory, memory[i+2], m2)
-			i += 4
-		} else if op == 3 {
-			memory[memory[i+1]] = input // assumed position mode
-			i += 2
-		} else if op == 4 {
-			output = getValue(memory, memory[i+1], m1)
-			i += 2
-		} else if op == 5 {
-			if getValue(memory, memory[i+1], m1) != 0 {
-				i = getValue(memory, memory[i+2], m2)
-			} else {
-				i += 3
-			}
-		} else if op == 6 {
-			if getValue(memory, memory[i+1], m1) == 0 {
-				i = getValue(memory, memory[i+2], m2)
-			} else {
-				i += 3
-			}
-		} else if op == 7 {
-			if getValue(memory, memory[i+1], m1) < getValue(memory, memory[i+2], m2) {
-				memory[memory[i+3]] = 1
-			} else {
-				memory[memory[i+3]] = 0
-			}
-			i += 4
-		} else if op == 8 {
-			if getValue(memory, memory[i+1], m1) == getValue(memory, memory[i+2], m2) {
-				memory[memory[i+3]] = 1
-			} else {
-				memory[memory[i+3]] = 0
-			}
-			i += 4
-		} else if op == 99 {
-			break
-		}
+func parseIntcode(file string) (intcode []int) {
+	input := utils.ReadFileToString(file)
+	inputData := strings.Split(input, ",")
+	for _, code := range inputData {
+		intcode = append(intcode, utils.Atoi(code))
 	}
 	return
 }
 
 func run(filepath string) (result, result2 int) {
-	input := utils.ReadFileToString(filepath)
-	inputData := strings.Split(input, ",")
-	var intcode []int
-	for _, code := range inputData {
-		intcode = append(intcode, utils.Atoi(code))
-	}
+	intcode := parseIntcode(filepath)
 
-	memory := append([]int(nil), intcode...)
-	result = runProg(memory, 1)
+	p := program.New(intcode)
+	p.Run([]int{1})
+	o := p.GetOutput()
+	result = o[len(o)-1]
 
-	memory = append([]int(nil), intcode...)
-	result2 = runProg(memory, 5)
+	p = program.New(intcode)
+	p.Run([]int{5})
+	result2 = p.PopOutput()
 
 	return
 }
